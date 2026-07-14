4b07e25d4979d254380da1dd4b9b49b8260b2d4b
Manjeet Singh <manjeetsingh9074@gmail.com>
2026-07-14 17:38:11 +0530

    fix: move play schema changes into migration

--- src/modules/mobile-auth/mobile-auth.service.ts
+++ src/modules/mobile-auth/mobile-auth.service.ts
@@ -93,9 +93,9 @@ export class MobileAuthService {
   }
 
   private assertStrongPassword(password: string) {
-    if (!/^(?=.*[^A-Za-z0-9])\S{8}$/.test(password)) {
+    if (!/^(?=.*[A-Z])(?=.*[^A-Za-z0-9])\S{8,}$/.test(password)) {
       throw new BadRequestException(
-        'Password must be exactly 8 characters long and include one special character',
+        'Password must be at least 8 characters long and include one capital letter and one special character',
       );
     }
   }
